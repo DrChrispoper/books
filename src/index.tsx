231d38312982982f446b0a8fec38ab7bce166ec2
Christopher Hockley <105942@supinfo.com>
2020-11-07 11:01:04 +0000

    refactor: :recycle: Updated default style and packages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import './assets/css/vendor/bootstrap.min.css';
-import './assets/css/vendor/bootstrap.rtl.only.min.css';
+import './assets/css/sass/themes/root.default.scss';
 
 const App = React.lazy(() => import(/* webpackChunkName: "App" */ './App'));
 
